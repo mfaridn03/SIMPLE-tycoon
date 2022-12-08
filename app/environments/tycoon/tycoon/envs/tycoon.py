2e7b88c33a725d34a2a3333a59a6b96912adc7cc
Farid <brentwoodfarid@gmail.com>
2022-12-08 14:07:25 +0800

    added documentation to some funcs

--- app/environments/tycoon/tycoon/envs/tycoon.py
+++ app/environments/tycoon/tycoon/envs/tycoon.py
@@ -34,18 +34,54 @@ class TycoonEnv(gym.Env):
         self.verbose = verbose
 
     def step(self, action):
+        """
+        From the docs:
+
+        The step method accepts an action from the current active player
+        and performs the necessary steps to update the game environment.
+        It should also it should update the current_player_num to the
+        next player, and check to see if an end state
+        of the game has been reached.
+        """
         pass
 
     def reset(self):
+        """
+        From the docs:
+
+        The reset method is called to reset the game to the starting state,
+        ready to accept the first action.
+        """
         self.wrapper.reset()
 
     def render(self, mode="human", close=False):
+        """
+        From the docs:
+
+        The render function is called to output a visual or
+        human readable summary of the current game state to the log file.
+        """
         pass
 
     def observation(self):
+        """
+        From the docs:
+
+        The observation function returns a numpy array that can be fed as input
+        to the PPO policy network. It should return a numeric representation
+        of the current game state, from the perspective of the current player,
+        where each element of the array is in the range [-1,1].
+        """
         pass
 
     def legal_actions(self):
+        """
+        From the docs:
+
+        The legal_actions function returns a numpy vector of the same
+        length as the action space, where 1 indicates that the
+        action is valid and 0 indicates that the action is invalid.
+        """
         pass
 
 
